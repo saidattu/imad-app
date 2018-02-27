02bc2265925e6d8642b4ea3b8fa6241df6d9e733
saidattu <36633451+saidattu@users.noreply.github.com>
2018-02-27 21:46:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img=document.getElementById('madi');
-madi.onclick=function()
+img.onclick=function()
 {
-    madi.style.marginLeft='100px';
+    img.style.marginLeft='100px';
 }
