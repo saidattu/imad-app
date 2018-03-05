9eb3c965816a9d20e499571c37c69d2a85d5a75e
saidattu <36633451+saidattu@users.noreply.github.com>
2018-03-05 23:54:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,17 @@ button.onclick=function () {
     request.open('GET','http://csaidattu77.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+// submit name
+var nameinput=document.getElementById('name');
+var name=nameinput.value;
+var submit=document.getElementById('submitbtn');
+submit.onclick=function(){
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list=list+'<li>'+names[i]+'</li>';
+    }
+};
