d88c72277c4bcfd8d6dc3826f8bc7a0b27b22224
saidattu <36633451+saidattu@users.noreply.github.com>
2018-03-07 23:22:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ submit.onclick=function(){
     var list='';
     for(var i=0;i<names.length;i++)
     {
-        list=list+'<li>'+names[i]+'</li>';
+        list+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('naamelist');
     ul.innerHTML=list;
